bc95c213ddd9c2b7eb74bdd25156052478446e4c
cube26 <rechie26@gmail.com>
2017-03-30 20:33:46 +0530

    functions

--- main.cpp
+++ main.cpp
@@ -12,9 +12,41 @@ extern "C" {
 // using get_time = chrono::steady_clock;
 
 float wheel_radius = 0.079259/2; 
-float max_speed = 1; // m/s
-float max_accel = 1;
-float max_brake = 1; // m/(s*s)  
+float max_speed = 1.0; // m/s
+float max_accel = 1.0;
+float max_brake = 1.0; // m/(s*s)
+int steeringLeftHandle, steeringRightHandle,motorLeftHandle,motorRightHandle, targethandle, GPS ;  
+
+void accelerate(int ID ,float accel_input,float angular_vel, float current_vel){
+	if(current_vel < max_speed){
+        angular_vel = angular_vel + ((accel_input*0.005)/wheel_radius);
+    	//std::cout <<"angular_vel " << angular_vel << std::endl;
+    }
+    else
+        angular_vel = angular_vel;
+    
+    simxSetJointTargetVelocity(ID,motorLeftHandle,angular_vel,simx_opmode_oneshot_wait);            
+    simxSetJointTargetVelocity(ID,motorRightHandle,angular_vel,simx_opmode_oneshot_wait);
+    //return(angular_vel);         	
+
+}
+void brake(int ID ,float brake_input,float angular_vel, float current_vel){
+	   if(angular_vel > 0)
+        {			
+           	angular_vel = angular_vel - ((brake_input*0.005)/wheel_radius);
+           	simxSetJointTargetVelocity(ID,motorLeftHandle,angular_vel,simx_opmode_oneshot_wait);            
+           	simxSetJointTargetVelocity(ID,motorRightHandle,angular_vel,simx_opmode_oneshot_wait);           
+ 		}
+ 		else
+ 		{
+ 			simxSetJointTargetVelocity(ID,motorLeftHandle,0,simx_opmode_oneshot_wait);            
+        	simxSetJointTargetVelocity(ID,motorRightHandle,0,simx_opmode_oneshot_wait);
+ 			//std::cout << "brake "<< curr_vel<< std::endl;
+ 			//simxStopSimulation(ID, simx_opmode_oneshot);
+ 		}             	
+ 		//return(angular_vel);
+}
+
 int main(int argc,char* argv[])
 {
     int clientID=simxStart((simxChar*)"127.0.0.1",19997,true,true,2000,5);
@@ -26,11 +58,11 @@ int main(int argc,char* argv[])
         //auto time_start = get_time::now();
         float curr_pos[7], targetpos[5];  // x,y,z,theta,vel 
         targetpos[4] = 0;
-        float accel = 1, brake = 1*100;
+        float accel = 199, deccel = 50;
 
-        float ang_vel = 0;  // angular velocity of wheel joint
-        float curr_vel = 0;
-        int steeringLeftHandle, steeringRightHandle,motorLeftHandle,motorRightHandle, targethandle, GPS ;  
+        float ang_vel = 0.0;  // angular velocity of wheel joint
+        float curr_vel = 0.0;
+        
         simxGetObjectHandle(clientID,"target", &targethandle, simx_opmode_oneshot_wait);
         simxGetObjectHandle(clientID,"GPS", &GPS, simx_opmode_oneshot_wait);
         simxGetObjectHandle(clientID,"nakedCar_steeringLeft", &steeringLeftHandle, simx_opmode_oneshot_wait); 
@@ -53,40 +85,21 @@ int main(int argc,char* argv[])
            
 
           // std::cout << " r " << r<< " alpha "<< alpha*180/3.14 << " Velocity :"<< curr_vel<<std::endl;
-		           
-
-
-
-
-           if(r >= 0.8 && curr_vel < 1.0) 
+		    if(r > 0.8 && curr_vel < 1.0) 
            {	
-           		if(ang_vel < max_speed/wheel_radius)
-           			ang_vel = ang_vel + accel*0.005/wheel_radius;
-           		else
-           			ang_vel = ang_vel;
-           		std::cout << "curr_vel "<< curr_vel<< std::endl;
-           		simxSetJointTargetVelocity(clientID,motorLeftHandle,ang_vel,simx_opmode_oneshot);            
-           		simxSetJointTargetVelocity(clientID,motorRightHandle,ang_vel,simx_opmode_oneshot);           
+    			
+    			accelerate(clientID ,accel , ang_vel, curr_vel);
+    			//std::cout << "curr_vel "<< curr_vel<< std::endl;       		  
            }
            else
            {	
-           		if(curr_vel != 0.0 && ang_vel>=0)
-           		{	
-           			
-           			
-           			ang_vel = ang_vel - brake*0.005/wheel_radius;
-           			std::cout << "curr_vel "<< curr_vel<< std::endl;
-           			simxSetJointTargetVelocity(clientID,motorLeftHandle,ang_vel,simx_opmode_oneshot);            
-           			simxSetJointTargetVelocity(clientID,motorRightHandle,ang_vel,simx_opmode_oneshot);           
- 				}
- 				// else
- 				// {
- 				// 	std::cout << "brake "<< curr_vel<< std::endl;
- 				// 	break;
- 				// }          
+           		//std::cout << "brake"<< curr_vel<< std::endl;
+           		brake(clientID ,deccel , ang_vel, curr_vel);
+           		
+           		
            }
             
-            extApi_sleepMs(5);
+            extApi_sleepMs(2);
         }
         simxFinish(clientID);
     }
